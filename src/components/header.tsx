41c3e28cebb8a9ce36d2089d3810190380f9edd8
gracianokaue <kauegraciano93@gmail.com>
2024-01-07 13:48:03 -0300

    Feita alterações no banner de informações

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,5 @@
 import styles from './header.module.css'
 import Menu from './menu'
-import inBtn from '../assets/linkedin2.png'
-import gitBtn from '../assets/github.png'
-import whatsBtn from '../assets/whatsapp.png'
 
 const Header = ()=> {
   return( 
@@ -20,14 +17,6 @@ const Header = ()=> {
         <nav className={styles.listContainer}>
 
         </nav>
-
-        <div className={styles.buttons}>
-
-            <div className={styles.contacts} id='contactLinkedin'><a href='https://www.linkedin.com/in/gracianokaue' target='_blank'> <img src={inBtn} /></a></div>
-            <div className={styles.contacts} id='contactGithub'><a href='https://github.com/gracianokaue/' target='_blank'> <img src={gitBtn} /></a></div>
-            <div className={styles.contacts} id='contactWhatsapp'><a href='https://wa.me/5541991597130' target='_blank'> <img src={whatsBtn} /></a></div>
-        
-        </div>
 
     </div>
 
--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,5 +1,7 @@
 import styles from './section.module.css'
-import author from '../assets/author.png'
+import inBtn from '../assets/linkedin2.png'
+import gitBtn from '../assets/github.png'
+import whatsBtn from '../assets/whatsapp.png'
 
 const Section = ()=> {
   return( 
@@ -13,11 +15,20 @@ const Section = ()=> {
             <div className={styles.sectionInfo}>
                 Sou Kauê Graciano, desenvolvedor web, confira aqui alguns dos meus projetos, aguardo seu contato!
             </div>
+
+            <div className={styles.buttons}>
+
+                <div className={styles.contacts} id='contactLinkedin'><a href='https://www.linkedin.com/in/gracianokaue' target='_blank'> <img src={inBtn} /></a></div>
+                <div className={styles.contacts} id='contactGithub'><a href='https://github.com/gracianokaue/' target='_blank'> <img src={gitBtn} /></a></div>
+                <div className={styles.contacts} id='contactWhatsapp'><a href='https://wa.me/5541991597130' target='_blank'> <img src={whatsBtn} /></a></div>
+
+            </div>
+
         </div>
 
-        <div className={styles.sectionPicture}>
+        <div className={styles.sectionPicture}></div>
 
-        </div>
+
     </section>
 );
 }
